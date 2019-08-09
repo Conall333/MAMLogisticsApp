4385ad45eaf1a5ffb784213cdd370129a351d67f
Conall333 <conallmccarthy@hotmail.com>
2019-08-09 06:18:55 +0100

    -- more testing

--- scripts/publishingTimeTesting.js
+++ scripts/publishingTimeTesting.js
@@ -7,7 +7,6 @@ modules_path = config.path;
 
 const Mam = require(path.join(modules_path,'mam','lib','mam.client.js'));
 const {asciiToTrytes, trytesToAscii} = require(path.join(modules_path,'converter'));
-const iota = require('iota.lib.js')
 
 // mam mode
 const mode = config.mode;
@@ -43,8 +42,7 @@ logger.warn(config.provider);
 
 // inititalize sate of mam
 
-mamState = Mam.init({provider,attachToTangle: myAttachFunction}, seed);
-
+mamState = Mam.init(provider, seed);
 
 //change to restricted mode
 mamState = Mam.changeMode(mamState, mode, channelKey);
